a782ed9df2c01038817728358454208902d59375
Lawrence Wu <lawrencewu@chromium.org>
2016-10-15 17:49:21 -0400

    add log

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
+  print("got a request")
   if request.args.get('hub.verify_token') == 'eyeshield' and request.args.get('hub.mode') == 'subscribe':
     print("verifying")
     return request.args.get('hub.challenge')
