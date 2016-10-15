a0b77063fda59edbf38a136500d7d28b2f6025f0
Lawrence Wu <lawrencewu@chromium.org>
2016-10-15 17:31:13 -0400

    add verification logic

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-  return "Hello World"
+  if request.args.get('hub.verify_token') == 'eyeshield' and request.args.get('hub.mode') == 'subscribe':
+    print("verifying")
+    return request.args.get('hub.challenge')
